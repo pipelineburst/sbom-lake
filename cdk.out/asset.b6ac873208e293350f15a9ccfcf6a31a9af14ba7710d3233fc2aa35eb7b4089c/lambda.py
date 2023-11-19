1a2b9d6f12d69f2025421b593a23923bc4d9a967
Dirk Michel <michel.dirk@googlemail.com>
2023-11-19 09:36:03 +0000

    initial project

---
+++ cdk.out/asset.b6ac873208e293350f15a9ccfcf6a31a9af14ba7710d3233fc2aa35eb7b4089c/lambda.py
@@ -0,0 +1,43 @@
+import json
+import boto3
+
+client = boto3.client('inspector2')
+
+def lambda_handler(event, context):
+    while True:
+        try:
+            txt = event['detail']['repository-name']
+            repo_name = txt.split("repository/")[-1]
+            print(repo_name)
+            response = client.create_sbom_export(
+                reportFormat='SPDX_2_3',
+                resourceFilterCriteria={
+                    'ecrRepositoryName': [
+                        {
+                            'comparison': 'EQUALS',
+                            'value': repo_name
+                        }
+                    ],
+                    'ecrImageTags': [
+                        {
+                        'comparison': 'EQUALS',
+                        'value': event['detail']['image-tags'][0]
+                        }
+                    ]
+                },
+                s3Destination={
+                    'bucketName': 'sbom-lake-u4jedu3',
+                    'kmsKeyArn': 'arn:aws:kms:eu-west-1:069127586842:key/f70a8a11-2181-478d-9798-fbfe9e52870a'
+                }
+            )
+        except Exception as e:
+            print('retrying...')
+            from time import sleep
+            sleep(5)
+            continue
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+        break
